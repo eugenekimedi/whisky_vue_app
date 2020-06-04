d4c2db6b0172fe95babc6dcce8a82efe2aacf0fc
Eugene Kim <eugenekim.edi@gmail.com>
2020-06-04 20:32:52 +0100

    Load regions in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <p>Hello World</p>
+    <p>Hello World</p>
 </template>
 
 <script>
+import RegionService from "@/services/regionService"
 export default {
-
+    name: "app",
+    data() {
+        return {
+            regions: [],
+            distilleries: [],
+            whiskies: [],
+        }
+    },
+    mounted() {
+        this.getRegions();
+    },
+    methods: {
+        getRegions() {
+            RegionService.getRegions()
+            .then (regions => this.regions = regions)
+        }
+    }
 }
 </script>
 
