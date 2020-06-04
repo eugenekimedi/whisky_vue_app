35a1e61022228ace01d1f695e2d4fbd6ef0e2cae
Eugene Kim <eugenekim.edi@gmail.com>
2020-06-04 20:46:40 +0100

    Add distillery-list component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>Hello World</p>
+    <div>
+        <h2>Distilleries</h2>
+        <distillery-list 
+            class="distillery-list" 
+            :distilleries='distilleries'
+        ></distillery-list>
+    </div>
 </template>
 
 <script>
 import RegionService from "@/services/regionService"
 import DistilleryService from "@/services/distilleryService"
 import WhiskyService from "@/services/whiskyService"
+import DistilleryList from "@/components/DistilleryList.vue"
 
 export default {
     name: "app",
@@ -34,6 +41,9 @@ export default {
             WhiskyService.getWhiskies()
             .then(whiskies => this.whiskies = whiskies)
         }
+    },
+    components: {
+        'distillery-list': DistilleryList,
     }
 }
 </script>
---
+++ client/src/components/DistilleryList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="distillery-list" v-if="distilleries.length">
+        <p v-for="distillery in distilleries">{{distillery.name}}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "distillery-list",
+    props: ["distilleries"]
+}
+</script>
+
+<style>
+.distillery-list {
+    display: inline-block;
+    background-color: #CC8E69;
+}
+</style>
